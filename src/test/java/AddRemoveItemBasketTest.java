54721fbc88cd6af70e31d24de9e80e053bacb341
Clinsen <clinsenmain@gmail.com>
2024-04-29 07:02:53 +0300

    Minor changes I should've committed instantly, because now I have but a single clue what in the world did I do there

--- src/test/java/AddRemoveItemBasketTest.java
+++ src/test/java/AddRemoveItemBasketTest.java
@@ -23,8 +23,10 @@ public class AddRemoveItemBasketTest extends Hooks {
     public void addRemoveItem() throws IOException, InterruptedException {
         Homepage home = new Homepage();
 
+        // Close the cookie pop-up
         home.getCookie().click();
 
+        // If the sidebar is invisible to user - open it
         if (home.getSidebar().getAttribute("class").contains("inactive")){
             home.getToggle().click();
         }
@@ -32,31 +34,39 @@ public class AddRemoveItemBasketTest extends Hooks {
         JavascriptExecutor jse = (JavascriptExecutor) getDriver();
         jse.executeScript("arguments[0].scrollIntoView()", home.getTestStoreLink());
 
+        // Go to the web store
         waitForClickabilityOf(home.getTestStoreLink(), Duration.ofSeconds(10));
         home.getTestStoreLink().click();
 
+        // Click on the first product
         PageObjectBase poBase = new PageObjectBase();
         poBase.getFirstProduct().click();
 
+        // Increase the quantity, add it to the cart
         StoreProductPage productPage = new StoreProductPage();
         productPage.getQuantityIncreaseBtn().click();
         productPage.getAddToCartBtn().click();
 
+        // Click on continue shopping btn
         StoreContentPanel contentPanel = new StoreContentPanel();
         contentPanel.getContinueShoppingBtn().click();
 
+        // Change parameters of the product, add it to the cart
         Select option = new Select(productPage.getSizeOption());
         option.selectByVisibleText("XL");
         waitForClickabilityOf(productPage.getAddToCartBtn(), Duration.ofSeconds(10));
         productPage.getAddToCartBtn().click();
 
+        // Proceed to checkout page (cart)
         waitForStalenessOf(contentPanel.getProceedToCheckoutBtn(), Duration.ofSeconds(10));
         contentPanel.getProceedToCheckoutBtn().click();
 
+        // Remove an item from the cart
         ShoppingCart sCart = new ShoppingCart();
         sCart.getRemoveItemBtn(2).click();
         waitForInvisibilityOf(sCart.getRemoveItemBtn(2), Duration.ofSeconds(10));
 
+        // Assert that the item was removed by comparing how much we should pay overall
         Assert.assertEquals(sCart.getTotalAmountValue().getText(), "$45.24");
 
         System.out.println("Add/Remove item test has been simulated.");
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -20,8 +20,10 @@ public class LoginTest extends Hooks {
     public void SimulateLoginProcess() throws IOException, InterruptedException{
         Homepage home = new Homepage();
 
+        // Close the cookie pop-up
         home.getCookie().click();
 
+        // If the sidebar is invisible to user - open it
         if (home.getSidebar().getAttribute("class").contains("inactive")){
             home.getToggle().click();
         }
@@ -29,22 +31,28 @@ public class LoginTest extends Hooks {
         JavascriptExecutor jse = (JavascriptExecutor) getDriver();
         jse.executeScript("arguments[0].scrollIntoView()", home.getTestStoreLink());
 
+        // Go to the web store
         waitForClickabilityOf(home.getTestStoreLink(), Duration.ofSeconds(10));
         home.getTestStoreLink().click();
 
+        // Click on sign in btn
         HeaderElements header = new HeaderElements();
         waitForClickabilityOf(header.getSignInBtn(), Duration.ofSeconds(10));
         header.getSignInBtn().click();
 
+        // This data should be hidden, ideally
         // Only for demo purposes
         String login = "testing123@test.com";
         String pass = "Testing!123";
 
+        // Enter data, submit it
         LoginPage lPage = new LoginPage();
         lPage.getEmailField().sendKeys(login);
         lPage.getPasswordField().sendKeys(pass);
         lPage.getSubmitBtn().click();
 
+        // Assert that we're logged in
+        waitForVisibilityOf(header.getSignOutBtn(), Duration.ofSeconds(10));
         Assert.assertTrue(header.getSignOutBtn().isDisplayed());
 
         System.out.println("Login test has been simulated.");
