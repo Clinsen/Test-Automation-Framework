117c97ce70fce687f31ace01850618b35126dc61
Clinsen <clinsenmain@gmail.com>
2024-05-13 21:34:48 +0300

    Create accountCreateBtn locator

--- src/main/java/pageObjects/LoginPage.java
+++ src/main/java/pageObjects/LoginPage.java
@@ -14,6 +14,7 @@ public class LoginPage extends BaseClass {
     By emailField = By.cssSelector("input#field-email");
     By pwdField = By.cssSelector("input#field-password");
     By submitBtn = By.cssSelector("button#submit-login");
+    By accountCreateBtn = By.cssSelector("[data-link-action='display-register-form']");
 
     public WebElement getEmailField() throws IOException {
         this.driver = getDriver();
@@ -23,8 +24,12 @@ public class LoginPage extends BaseClass {
         this.driver = getDriver();
         return driver.findElement(pwdField);
     }
-    public WebElement getSubmitBtn() throws IOException{
+    public WebElement getSubmitBtn() throws IOException {
         this.driver = getDriver();
         return driver.findElement(submitBtn);
+    }
+    public WebElement getAccountCreateBtn() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(accountCreateBtn);
     }
 }
