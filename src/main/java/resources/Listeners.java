629342b9d01c576bf7046ee68187179b2c0920ec
Clinsen <clinsenmain@gmail.com>
2024-05-13 20:51:24 +0300

    BasePage was renamed to BaseClass

--- src/main/java/resources/Listeners.java
+++ src/main/java/resources/Listeners.java
@@ -1,12 +1,12 @@
 package resources;
 
-import base.BasePage;
+import base.BaseClass;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.io.IOException;
 
-public class Listeners extends BasePage implements ITestListener {
+public class Listeners extends BaseClass implements ITestListener {
     public Listeners() throws IOException {
         super();
     }
